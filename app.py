52884bf39bcbb5483104bdfde511b31f37186054
rexsimiloluwah <38814629+rexsimiloluwah@users.noreply.github.com>
2020-04-16 03:44:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,26 @@ import gensim
 import nltk
 from nltk import word_tokenize, sent_tokenize
 import re
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from gensim.summarization import summarize
 import PyPDF2
 import requests
 from bs4 import BeautifulSoup
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 app = Flask(__name__)
+
+stopwords = ['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him',
+ 'his','himself','she',"she's",'her','hers','herself','it',"it's",'its','itself','they','them','their','theirs','themselves','what','which',
+ 'who','whom','this','that',"that'll",'these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did',
+ 'doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into',
+  'through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then',
+ 'once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only',
+ 'own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d','ll','m','o','re','ve','y',
+ 'ain','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",
+ 'ma','mightn',"mightn't",'mustn', 'needn',"needn't",'shan',"shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won',"won't",
+ 'wouldn',"wouldn't"]
 
 def readPdf(file_upload):
     pdf_content = PyPDF2.PdfFileReader(open(file_upload,'rb'))
@@ -35,14 +46,14 @@ def gensim_summarize(rawtext):
     
     word_frequencies = {}
     for word in nltk.word_tokenize(re.sub('[^a-zA-Z]',' ',formatted_text)):
-        if word not in nltk.corpus.stopwords.words('english'):
+        if word not in stopwords:
             if word not in word_frequencies.keys():
                 word_frequencies[word] = 1
             else:
                 word_frequencies[word] += 1
     #keywords extraction
 
-    keywords = [word for word in list(dict(sorted(word_frequencies.items(), key= lambda x:x[1], reverse=True)).keys()) if word.lower() not in nltk.corpus.stopwords.words('english') and len(word) >= 3][:7]
+    keywords = [word for word in list(dict(sorted(word_frequencies.items(), key= lambda x:x[1], reverse=True)).keys()) if word.lower() not in stopwords and len(word) >= 3][:7]
     summarized = ' '.join(summarize(formatted_text).splitlines())
     
     return(len(rawtext), len(summarized), summarized, keywords)
@@ -53,14 +64,14 @@ def get_keywords(rawtext):
     
     word_frequencies = {}
     for word in nltk.word_tokenize(re.sub('[^a-zA-Z]',' ',formatted_text)):
-        if word not in nltk.corpus.stopwords.words('english'):
+        if word not in stopwords:
             if word not in word_frequencies.keys():
                 word_frequencies[word] = 1
             else:
                 word_frequencies[word] += 1
     #keywords extraction
 
-    keywords = [word for word in list(dict(sorted(word_frequencies.items(), key= lambda x:x[1], reverse=True)).keys()) if word.lower() not in nltk.corpus.stopwords.words('english') and len(word) >= 3][:7]
+    keywords = [word for word in list(dict(sorted(word_frequencies.items(), key= lambda x:x[1], reverse=True)).keys()) if word.lower() not in stopwords and len(word) >= 3][:7]
     return keywords
 
 
